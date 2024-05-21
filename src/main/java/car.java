3d69cf81cfdab350afde6cd71b3e3e5a2c51fce9
YounesAtGithub <younesel@gmail.com>
2024-05-21 10:39:37 +0200

    first commit

---
+++ src/main/java/car.java
@@ -0,0 +1,17 @@
+public class car {
+    String brand;
+    String model;
+    int year;
+    String color;
+    double engineSize;
+
+    public car(String brand, String model, int year, String color, double engineSize) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.engineSize = engineSize;
+    }
+
+
+}
