0a833559e7e10e9df7faf4190f3eb0b2ba82e1ca
YounesAtGithub <younesel@gmail.com>
2024-05-21 23:21:44 +0200

    gebleven bij stap 4, onduidelijk waarom symbol niet resolved kan worden

---
+++ src/main/java/Car.java
@@ -0,0 +1,25 @@
+class Car {
+    String brand;
+
+
+    /*  String model;
+    int year;
+    String color;
+    double engineSize;
+*/
+
+    /*    public Car(String brand /*, String model, int year, String color, double engineSize)*/ /*{
+        this.brand = brand;
+      this.model = model;
+        this.year = year;
+        this.color = color;
+        this.engineSize = engineSize;
+
+    }
+*/
+    public static void printCarFacts() {
+        System.out.println("Deze");
+
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,16 @@
 public class Main {
-    printCarInfo();
 
     public static void main(String[] args) {
-        car fiat = new car("Fiat", "500", 2019, "Groen", 1.4);
-        car toyota = new car("Toyota", "Aygo", 2016, "Blauw", 1.2);
-        car volvo = new car("Volvo", "CX90", 2022, "Zwart", 2.0);
+        Car auto = new Car();
+
+/*      Car fiat = new Car("Fiat", "500", 2019, "Groen", 1.4);
+        Car toyota = new Car("Toyota", "Aygo", 2016, "Blauw", 1.2);
+        Car volvo = new Car("Volvo", "CX90", 2022, "Zwart", 2.0);
+*/
     }
 
-    public static void printCarInfo() {
-        System.out.println("Deze" + brand + model + " is " + color);
+    public static void print(String[] args) {
+        System.out.println();
     }
+
 }
